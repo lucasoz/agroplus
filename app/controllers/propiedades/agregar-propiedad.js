d30f35f149c2598896b37f3975947fe563c54ef3
jxebastian <josrodriguezca@unal.edu.co>
2018-04-19 15:42:48 -0500

    Validaciones agregar propiedad listo,solo falta poner los mensajesBonito

--- app/controllers/propiedades/agregar-propiedad.js
+++ app/controllers/propiedades/agregar-propiedad.js
@@ -20,21 +20,61 @@ export default Controller.extend({
       municipio == '' || municipio == undefined) {
         alert('Completa todo los campos');
       }else {
-        ///Guardar la propiedad
-        var usuario = this.model;
-        var nuevaPropiedad = this.store.createRecord('propiedad',{
-          nombre: nombrePropiedad,
-          longitud: longitud,
-          latitud: latitud,
-          departamento: departamento,
-          municipio: municipio,
-          vereda: vereda,
-          usuario: usuario
-        });
-        usuario.get('propiedades').addObject(nuevaPropiedad);
-        nuevaPropiedad.save().then(function(){
-          return usuario.save();
-        });
+        ///valida latitud -90,90
+        if(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/.test(latitud)){
+          ///validad longitud -180,180
+          if (/^[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/.test(longitud)) {
+            ///valida que nombrePropiedad solo tenga letras
+            if (/^[A-Za-z ]+$/.test(nombrePropiedad)) {
+              /// valida que departamento solo tenga letras
+              if (/^[A-Za-z ]+$/.test(departamento)) {
+                /// valida que municipio solo letras
+                if (/^[A-Za-z ]+$/.test(municipio)) {
+                  ///valida que vereda solo letras
+                  if (/^[A-Za-z ]*$/.test(vereda)) {
+                    /// guardad datos
+                    var usuario = this.model;
+                    var nuevaPropiedad = this.store.createRecord('propiedad',{
+                      nombre: nombrePropiedad,
+                      longitud: longitud,
+                      latitud: latitud,
+                      departamento: departamento,
+                      municipio: municipio,
+                      vereda: vereda,
+                      usuario: usuario
+                    });
+                    usuario.get('propiedades').addObject(nuevaPropiedad);
+                    nuevaPropiedad.save().then(function(){
+                      return usuario.save();
+                    });
+
+                    /// limpiar campos
+                    this.set('nombrePropiedad', '');
+                    this.set('latitud', '');
+                    this.set('longitud', '');
+                    this.set('departamento', '');
+                    this.set('municipio', '');
+                    this.set('vereda', '');
+                    alert('Se agrego la propiedad');
+                  }else {
+                    alert('Vereda debe tener solo letras');
+                  }
+                }else {
+                  alert('Municipio debe tener solo letras');
+                }
+              }else{
+                alert('Departamento debe tener solo letras');
+              }
+            }else{
+              alert('Nombre de propiedad debe tener solo letras');
+            }
+          }else {
+            alert('Longitud no valido');
+          }
+        }else{
+          alert('Latitud no valido');
+        }
+
       }
     }
   }
