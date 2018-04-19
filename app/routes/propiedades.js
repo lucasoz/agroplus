b8f6c319cffdc285c3682e119dda52fd065a58a9
jxebastian <josrodriguezca@unal.edu.co>
2018-04-19 18:20:28 -0500

    solucionando un problema

--- app/routes/propiedades.js
+++ app/routes/propiedades.js
@@ -1,7 +1,21 @@
 import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
 
 export default Route.extend({
+  firebaseApp: inject(),
   model(){
-    return this.store.findAll('propiedad');
+    var usuarioAutenticado = this.get('firebaseApp').auth().currentUser;
+    //console.log(usuarioAutenticado.uid);
+    return this.get('store').findRecord('usuario', usuarioAutenticado.uid);
+    /*
+    this.get('firebaseApp').auth().onAuthStateChanged((user)=> {
+      if (user) {
+        console.log(user.uid);
+        return this.get('store').findRecord('usuario', user.uid);
+      } else {
+        // No user is signed in.
+      }
+    });
+    */
   }
 });
